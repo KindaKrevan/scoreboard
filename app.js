67da2c9cf3dae417359b4e4691fde956ff5f7d2c
Kyle Evans <krevan88@protonmail.com>
2021-12-06 14:18:48 -0700

    buttons and scoreboard working

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World.");
+let score1 = 0;
+
+function addScore() {
+  score1++;
+  document.getElementById("points1").innerText = score;
+}
+
+function remScore() {
+  score1--;
+  document.getElementById("points1").innerHTML = score;
+}
